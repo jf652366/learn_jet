bae1000348f6d4efed72b1dc47ed36c2ab560418
fengjin <3028446915@qq.com>
2022-01-12 22:38:05 +0800

    feat(src/App.test.tsx): 测试

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          222 Learn React
+          hello word
         </a>
       </header>
     </div>
