84a4a1b6282471436067fc1f3e4ec35cc8b35a92
fengjin <3028446915@qq.com>
2022-01-13 20:57:40 +0800

    feat(src/screens/project-list/index.jsx): 添加工程列表组件

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,9 +1,35 @@
 import {SearchPanel} from "./search-panel";
 import {List} from "./list";
-
+import { useEffect, useState} from "react";
+import * as qs from 'qs'
+import {clearnObject} from "../../utils";
+const apiUrl=process.env.REACT_APP_API_URL
 export const ProjectListScreen=()=>{
+    const [param,setParam]=useState({
+        name:'',
+        personId:''
+    })
+    const [list,setList]=useState([])
+    const [users,setUsers]=useState([]);
+    useEffect(()=>{
+        fetch(`${apiUrl}/projects?${qs.stringify(clearnObject(param))}`).then(async response=>{
+            if (response.ok){
+                setList(await response.json())
+
+
+
+            }
+        })
+    },[param])
+    useEffect(()=>{
+        fetch(`${apiUrl}/users`).then(async response=>{
+            if (response.ok){
+                setUsers(await response.json())
+            }
+        })
+    },[])
     return <div>
-        <SearchPanel/>
-        <List/>
+        <SearchPanel users={users} param={param} setParam={setParam}/>
+        <List users={users} list={list}/>
     </div>
 }
