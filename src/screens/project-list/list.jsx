4b0a1ce20ce56955e687dfea2ec353d5a5ad176b
fengjin <3028446915@qq.com>
2022-01-13 20:58:20 +0800

    feat(src/screens/project-list/list.jsx): 添加工程列表的列表组件

--- src/screens/project-list/list.jsx
+++ src/screens/project-list/list.jsx
@@ -1,4 +1,19 @@
-export const List=()=>{
-    return <div> </div>
-    
+export const List = ({list, users}) => {
+    return <table>
+        <thead>
+        <tr>
+            <th>名称</th>
+            <th>负责人</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+            list.map(project => <tr key={project.id}>
+                <td>{project.name}</td>
+                <td>{users.find(user=>project.personId===user.id)?.name}</td>
+            </tr>)
+        }
+        </tbody>
+    </table>
+
 }
