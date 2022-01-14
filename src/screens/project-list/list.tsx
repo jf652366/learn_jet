8be528dd837074fcb4d4ebd0d876bebfc2b9e865
fengjin <3028446915@qq.com>
2022-01-14 11:22:28 +0800

    chore(src/screens/project-list/list.tsx): 添加tsl类型定义

---
+++ src/screens/project-list/list.tsx
@@ -0,0 +1,32 @@
+import { User } from "./search-panel";
+interface Project {
+  id: string;
+  name: string;
+  personId: string;
+  pin: boolean;
+  organization: string;
+}
+interface listProps {
+  list: Project[];
+  users: User[];
+}
+export const List = ({ list, users }: listProps) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>名称</th>
+          <th>负责人</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map((project) => (
+          <tr key={project.id}>
+            <td>{project.name}</td>
+            <td>{users.find((user) => project.personId === user.id)?.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
