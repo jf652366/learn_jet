27d6bbd4620a67f65dae5c4e16e885d8894dd031
fengjin <3028446915@qq.com>
2022-02-08 07:41:12 +0800

    feat(src/screens/project-screen): 完善projectScreen 页面

--- src/screens/project-screen/index.tsx
+++ src/screens/project-screen/index.tsx
@@ -1,3 +1,28 @@
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router";
+import { KanbanScreen } from "../kanban-screen";
+import { EpicScreen } from "../epic-screen";
+
+import React from "react";
+
 export const ProjectScreen = () => {
-  return <div>ProjectScreen</div>;
+  return (
+    <div>
+      <h1>ProjectScreen</h1>
+      <ul>
+        <li>
+          <Link to={"kanban"}>看板</Link>
+        </li>
+        <li>
+          <Link to={"epic"}>任务组</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route index element={<KanbanScreen />} />
+        <Route path={"kanban"} element={<KanbanScreen />} />
+        <Route path={"epic"} element={<EpicScreen />} />
+      </Routes>
+    </div>
+  );
 };
